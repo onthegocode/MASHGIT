30165df20a80acc4349c3c9af5b8c30ace6529e7
Andrew <andrewnyarivera@gmail.com>
2018-07-13 18:52:30 -0400

    mashgit

---
+++ Mashgit.js
@@ -0,0 +1,42 @@
+//
+function Mash() {
+    return "You will live in a: " + getHome() + " and you will have: " + getChildrenCount() + " kids"+" and you'll drive in a: "+getCar();
+}
+console.log(Mash());
+
+//Homes
+function getHome(){
+    let x = process.argv[2];
+const randomly = ["Mansion","Apartment","Shack", "House", "Pool Boy"];
+randomly.push(x);
+let home = Math.floor(randomly.length * Math.random());
+let homes = randomly[home];
+return homes
+
+}
+
+//Number of child
+function getChildrenCount(){
+    let x = process.argv[3];
+    
+    let child = Math.floor(Math.random() * 100);
+    if (Math.floor(Math.random()* 2) == 1 ){
+        return child;
+    }
+    else if (process.argv[3] == undefined){
+        return child;
+
+    
+    }
+    }
+
+
+function getCar(){
+    process.arg
+    const cars = ["Lambo","Convertible","box with wheels","mom car"];
+    let car = Math.floor(cars.length * Math.random());
+    let mycar = cars[car];
+    return cars;
+}
+
+
